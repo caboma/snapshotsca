af97159a067f99a4ac4017c4b9eac3b751a28068
Omar Cabatbat <omar_cabatbat@yahoo.com>
2023-04-20 22:19:23 -0400

    Setting up project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Home from './pages/home/Index'
+import About from './pages/about/About'
+import Contacts from './pages/contacts/Contacts'
+import Portfolio from './pages/portfolio/Portfolio';
+import Services from './pages/services/Services';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Home />
+      <About />
+      <Contacts />
+      <Portfolio />
+      <Services />
     </div>
   );
 }
